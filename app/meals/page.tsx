d3f1155ea884cf88375e0dd85197da0094b3c869
VadimKorobec <_s_e_t__@ukr.net>
2024-05-09 14:48:07 +0300

    Create page.tsx

---
+++ app/meals/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const MealsPage = () => {
+  return (
+    <main>
+      <h1>Meals Page</h1>
+      <Link href="/meals/share">share</Link>
+    </main>
+  );
+};
+
+export default MealsPage;
