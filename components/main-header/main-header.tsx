206963175ce9a577dcb473a80eec352e89758781
VadimKorobec <_s_e_t__@ukr.net>
2024-05-10 15:15:24 +0300

    creare nav

--- components/main-header/main-header.tsx
+++ components/main-header/main-header.tsx
@@ -5,6 +5,7 @@ import logoImg from "@/assets/logo.png";
 import MainHeaderBackground from "./main-header-background";
 
 import styles from "./main-header.module.css";
+import NavLink from "../nav/nav-link";
 
 const MainHeader = () => {
   return (
@@ -18,10 +19,10 @@ const MainHeader = () => {
         <nav className={styles.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href={"/meals"}>Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href={"/community"}>Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
---
+++ components/nav/nav-link.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React, { useEffect } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import styles from "./nav-link.module.css";
+
+interface NavLinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const NavLink = ({ href, children }: NavLinkProps) => {
+  const path = usePathname();
+
+  return (
+    <Link href={href} className={path === href ? styles.active : ""}>
+      {children}
+    </Link>
+  );
+};
+
+export default NavLink;
