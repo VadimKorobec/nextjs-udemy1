78c90f8203d69d7a2a32bc96a310db934738bb8b
VadimKorobec <_s_e_t__@ukr.net>
2024-05-11 18:02:17 +0300

    add form

--- lib/meals.ts
+++ lib/meals.ts
@@ -2,25 +2,23 @@ import sql from "better-sqlite3";
 
 const db = sql("meals.db");
 
-export async function getMeals(): Promise<
-  Array<{
-    title: string;
-    slug: string;
-    image: string;
-    instructions: string;
-    creator: string;
-    creator_email: string;
-    summary: string;
-  }>
-> {
+interface Meal {
+  title: string;
+  slug: string;
+  image: string;
+  instructions: string;
+  creator: string;
+  creator_email: string;
+  summary: string;
+}
+
+export async function getMeals(): Promise<Meal[]> {
   await new Promise((resolve) => setTimeout(resolve, 5000));
-  return db.prepare("SELECT * FROM meals").all() as Array<{
-    title: string;
-    slug: string;
-    image: string;
-    instructions: string;
-    creator: string;
-    creator_email: string;
-    summary: string;
-  }>;
+  return db.prepare("SELECT * FROM meals").all() as Meal[];
+}
+
+export function getMeal(slug: string) {
+  const stmt = db.prepare("SELECT * FROM meals WHERE slug = ?");
+  const meal = stmt.get(slug) as Meal;
+  return meal;
 }
