cb4e197830b898294b83872df98cfa1e60b4d27a
VadimKorobec <_s_e_t__@ukr.net>
2024-05-14 17:31:10 +0300

    add some code

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,4 +1,5 @@
 import ImagePicker from "@/components/meals/image-picker";
+import { shareMeal } from "@/lib/action";
 
 import styles from "./page.module.css";
 
@@ -12,7 +13,7 @@ const ShareMealPage = () => {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form}>
+        <form className={styles.form} action={shareMeal}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -40,7 +41,7 @@ const ShareMealPage = () => {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={styles.actions}>
             <button type="submit">Share Meal</button>
           </p>
--- lib/meals.ts
+++ lib/meals.ts
@@ -1,4 +1,8 @@
+import fs from "node:fs";
+
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
 
 const db = sql("meals.db");
 
@@ -12,13 +16,26 @@ interface Meal {
   summary: string;
 }
 
-export async function getMeals(): Promise<Meal[]> {
+export const getMeals = async (): Promise<Meal[]> => {
   await new Promise((resolve) => setTimeout(resolve, 5000));
   return db.prepare("SELECT * FROM meals").all() as Meal[];
-}
+};
 
-export function getMeal(slug: string) {
+export const getMeal = (slug: string) => {
   const stmt = db.prepare("SELECT * FROM meals WHERE slug = ?");
   const meal = stmt.get(slug) as Meal;
   return meal;
-}
+};
+
+export const saveMeal = async (meal: any) => {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split(".").pop();
+  const fileName = `${meal.slug}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage));
+};
